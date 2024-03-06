12e078b31de8c135f5cf1dac34255ad6f941f4b7
username <lara.viquiera@gmail.com>
2024-03-05 22:30:20 -0800

    fix ensureAdmin

--- config/ensureAdmin.js
+++ config/ensureAdmin.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
     // use status code of 401 unauthorized
-    if (!req.user.admin) return res.status(401).json('Unauthorized');
+    if (!req.user || !req.user.admin) return res.status(401).json('Unauthorized');
     // A okay
     next();
 };
